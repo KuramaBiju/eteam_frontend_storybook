1d5a13f671a2bd7377c808ac0339a4f173d8b8a7
Baby Kurama <73858430+KuramaBiju@users.noreply.github.com>
2023-02-12 16:34:43 -0300

    Change main.cjs, and add addons

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,12 +1,14 @@
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
+    '../src/**/*.stories.tsx',
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-cssresources"
   ],
   "framework": "@storybook/react",
   "core": {
