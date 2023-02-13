fa031bdf50702e50672034612970b7be269312e3
Baby Kurama <73858430+KuramaBiju@users.noreply.github.com>
2023-02-13 19:46:44 -0300

    Add: Address story with default template

---
+++ src/stories/components/company/Address.stories.tsx
@@ -0,0 +1,16 @@
+import { Address } from "../../../components/Company";
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+export default {
+    title:'Address',
+    component: Address
+} as ComponentMeta<typeof Address>;
+
+const Template: ComponentStory<typeof Address> = (args) => <Address {...args} />;
+
+
+export const Default = Template.bind({});
+Default.args = {
+    address:"Direcc"
+};
+                        
