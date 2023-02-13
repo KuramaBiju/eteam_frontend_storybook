669298fc9199e0d0c5b230b3d732af5b960cd37e
Baby Kurama <73858430+KuramaBiju@users.noreply.github.com>
2023-02-13 19:45:49 -0300

    Refactor tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content:["./src/**/*.{ts,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
- 
+    extend: {},
   },
   plugins: [],
 }
