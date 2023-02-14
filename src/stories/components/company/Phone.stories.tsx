ff6382444d2632301f2a27f14d496c0e01de3548
Baby Kurama <73858430+KuramaBiju@users.noreply.github.com>
2023-02-13 21:58:43 -0300

    Add: Phone story

---
+++ src/stories/components/company/Phone.stories.tsx
@@ -0,0 +1,16 @@
+import { Phone } from "../../../components/Company";
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+export default {
+    title:'Phone',
+    component: Phone
+} as ComponentMeta<typeof Phone>;
+
+const Template: ComponentStory<typeof Phone> = (args) => <Phone {...args} />;
+
+
+export const Default = Template.bind({});
+Default.args = {
+    phone:"345678910"
+};
+                        
