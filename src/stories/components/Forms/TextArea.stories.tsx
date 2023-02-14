2babcb802e38c3a7db9677c865a25c33f20e785a
Baby Kurama <73858430+KuramaBiju@users.noreply.github.com>
2023-02-13 21:59:45 -0300

    Add: TextArea story

---
+++ src/stories/components/Forms/TextArea.stories.tsx
@@ -0,0 +1,19 @@
+import { TextArea } from "../../../components/Forms";
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+export default {
+    title:'TextArea',
+    component: TextArea
+} as ComponentMeta<typeof TextArea>;
+
+const Template: ComponentStory<typeof TextArea> = (args) => <TextArea {...args} />;
+
+
+export const Default = Template.bind({});
+Default.args = {
+    label:"TextArea",
+    name:"default",
+    placeholder:"Teeeeexteando afafafafafaaf"
+    
+};
+                        
