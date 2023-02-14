8c41433eaff6b417c3d7182b4bb8f1d3dd2ad5b6
Baby Kurama <73858430+KuramaBiju@users.noreply.github.com>
2023-02-13 21:58:51 -0300

    Add: Web story

---
+++ src/stories/components/company/Web.stories.tsx
@@ -0,0 +1,16 @@
+import { Web } from "../../../components/Company";
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+export default {
+    title:'Web',
+    component: Web
+} as ComponentMeta<typeof Web>;
+
+const Template: ComponentStory<typeof Web> = (args) => <Web {...args} />;
+
+
+export const Default = Template.bind({});
+Default.args = {
+    web:"https://www.trik.com.br"
+};
+                        
