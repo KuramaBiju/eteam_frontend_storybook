7c38c77a9aef522ef9c6825a31f1fe34e2698afd
Baby Kurama <73858430+KuramaBiju@users.noreply.github.com>
2023-02-13 21:58:48 -0300

    Add: State story

---
+++ src/stories/components/company/State.stories.tsx
@@ -0,0 +1,16 @@
+import { State } from "../../../components/Company";
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+export default {
+    title:'State',
+    component: State
+} as ComponentMeta<typeof State>;
+
+const Template: ComponentStory<typeof State> = (args) => <State {...args} />;
+
+
+export const Default = Template.bind({});
+Default.args = {
+    state:"Buenos Aires BR"
+};
+                        
