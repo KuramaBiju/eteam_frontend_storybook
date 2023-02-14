84382aacefa3f3798f2c07f63d8c7b95a700799c
Baby Kurama <73858430+KuramaBiju@users.noreply.github.com>
2023-02-14 20:52:15 -0300

    Refactor: Button story, set title for the tabs in the sidebar

--- src/stories/components/Button/Button.stories.tsx
+++ src/stories/components/Button/Button.stories.tsx
@@ -1,8 +1,9 @@
 import { Button } from "../../../components/Button";
 import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 
 export default {
-    title:'Button',
+    title:'Buttons/Button',
     component: Button
 } as ComponentMeta<typeof Button>;
 
@@ -13,6 +14,41 @@ export const Login = Template.bind({});
 Login.args = {
     type:"button",
     text:"Login",
-    typeButton:"button-login"
+    typeButton:"button-login",
+    clickHandler: action('Login')
+};
+
+export const Pagination = Template.bind({});
+Pagination.args = {
+    type:"button",
+    text:"Next",
+    typeButton:"button-pagination",
+    clickHandler: action('Pagination')
+};
+
+export const Decline = Template.bind({});
+Decline.args = {
+    type:"button",
+    text:"Decline",
+    typeButton:"button-decline",
+    clickHandler: action('Decline')
 };
                         
+
+export const Accept = Template.bind({});
+Accept.args = {
+    type:"button",
+    text:"Accept",
+    typeButton:"button-accept",
+    clickHandler: action('Accept')
+};
+
+
+export const Submit = Template.bind({});
+Submit.args = {
+    type:"submit",
+    text:"Submit",
+    typeButton:"button-submit",
+    clickHandler: action('Submit')
+};
+
