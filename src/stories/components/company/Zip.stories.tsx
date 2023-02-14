4e3764067ed5a5fd7bb53e64b83916d8bc306298
Baby Kurama <73858430+KuramaBiju@users.noreply.github.com>
2023-02-13 21:58:57 -0300

    Add: Zip story

---
+++ src/stories/components/company/Zip.stories.tsx
@@ -0,0 +1,16 @@
+import { Zip } from "../../../components/Company";
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+export default {
+    title:'Zip',
+    component: Zip
+} as ComponentMeta<typeof Zip>;
+
+const Template: ComponentStory<typeof Zip> = (args) => <Zip {...args} />;
+
+
+export const Default = Template.bind({});
+Default.args = {
+    zip:"1900"
+};
+                        
