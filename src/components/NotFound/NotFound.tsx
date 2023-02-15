8a75ed72b6fed0124dc9c4c0ad344bf507979374
Baby Kurama <73858430+KuramaBiju@users.noreply.github.com>
2023-02-15 19:29:14 -0300

    Add: Props in NotFound component

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 
-export const NotFound = () => {
+interface Props {
+  className?: string;
+  style?: CSSProperties;
+}
+
+export const NotFound = ({className,style}: Props) => {
   return (
     <div className="flex items-center justify-center h-64">
-      <p className="text-gray-500 text-lg">No se encontraron elementos</p>
+      <p className={`text-gray-500 text-lg ${className}`} style={style}>No se encontraron elementos</p>
     </div>
   );
 };
