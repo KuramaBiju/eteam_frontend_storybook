775d93ab58a8e1922715aebd1cd749a36def5483
Baby Kurama <73858430+KuramaBiju@users.noreply.github.com>
2023-02-15 19:30:05 -0300

    Refactor: TagRole component, svg props

--- src/components/Tags/TagRole.tsx
+++ src/components/Tags/TagRole.tsx
@@ -18,9 +18,9 @@ export const TagRole = ({name,style,className} :Props ) => {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="feather feather-arrow-right mr-2"
     >
       <line x1="5" y1="12" x2="19" y2="12"></line>
